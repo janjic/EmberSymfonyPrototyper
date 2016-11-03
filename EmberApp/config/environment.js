5da418f3f923d5b8b24b3de45d7072f3ac73675e
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2016-11-03 13:59:28 +0100

    Some config changes

--- EmberApp/config/environment.js
+++ EmberApp/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-app',
     environment: environment,
-    rootURL: '/',
+    rootURL: '/app/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
