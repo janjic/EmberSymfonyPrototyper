4b1f035ed49b80c0aa26ffa2868036e449b122c2
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2016-11-03 14:34:13 +0100

    Commit

---
+++ EmberApp/app/adapter/application.js
@@ -0,0 +1,9 @@
+// app/adapter/application.js
+
+import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+export default DS.RESTAdapter.extend(DataAdapterMixin, {
+    namespace: 'api',
+    authorizer: 'authorizer:application'
+});
