41cc0dccaa9773cf1865ffed41b51a35bec54d27
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2016-11-03 09:38:10 +0100

    first commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+    * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/api/test", name="api_test")
+     */
+    public function apiTestAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return new JsonResponse(
+            array(
+                'type' => array(
+                    'id'=>1
+                )
+            )
+        );
+    }
+}
