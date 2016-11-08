09665cc19362d0a069aaa33dffada123b4a1557a
nemanja <nkm790021@gmail.com>
2016-11-08 14:45:05 +0100

    New pages

--- EmberApp/app/router.js
+++ EmberApp/app/router.js
@@ -18,6 +18,25 @@ Router.map(function() {
 
   this.route('dashboard', function() {
     this.route('users-all');
+    this.route('add-new-user');
+    this.route('add-new-agent');
+    this.route('all-agents');
+    this.route('agent-update-history');
+    this.route('tree-view');
+    this.route('table-view');
+    this.route('folder-view');
+    this.route('home');
+    this.route('payouts-to-agents');
+    this.route('reports');
+    this.route('add-new-mail-list');
+    this.route('all-mail-lists');
+    this.route('add-mm-campaign');
+    this.route('all-mm-campaigns');
+    this.route('received-messages');
+    this.route('drafts');
+    this.route('sent');
+    this.route('trash');
+    this.route('settings');
   });
 });
 
---
+++ EmberApp/tests/unit/routes/dashboard/add-new-mail-list-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:dashboard/add-new-mail-list', 'Unit | Route | dashboard/add new mail list', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
