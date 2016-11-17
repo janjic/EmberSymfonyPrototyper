08061ef3ac70b49e5fd52c70210a61882c225ffd
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2016-11-17 08:50:46 +0100

    Test

--- EmberApp/app/components/forgot-password.js
+++ EmberApp/app/components/forgot-password.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
+import ForgotPasswordValidations from '../validations/forgot-password';
 
 export default Ember.Component.extend({
+    validations: ForgotPasswordValidations,
+    email: '',
+    init() {
+        this._super(...arguments);
+    },
+    actions: {
+        reset(changeset) {
+            return changeset.rollback();
+        },
+
+        validateProperty(changeset, property) {
+            return changeset.validate(property);
+        }
+    }
 });
