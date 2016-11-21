8a4c31fd7c48749546e3aa54006824d3b232fac7
Nikola Ugrinovic <ugra1992@gmail.com>
2016-11-21 15:55:02 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	EmberApp/app/templates/components/table-view-component.hbs

--- EmberApp/app/components/agent/add-agent-component.js
+++ EmberApp/app/components/agent/add-agent-component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.Component.extend({
     actions: {
         updateAgentBirthDate(date){
             var agent = this.model;
--- EmberApp/app/components/agent/edit-agent-component.js
+++ EmberApp/app/components/agent/edit-agent-component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.Component.extend({
     store: Ember.inject.service(),
     actions: {
         roleSelected(group){
