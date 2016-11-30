f3e9afc5a2177c26e3dc51cd8b973302063e5faf
Nikola Ugrinovic <ugra1992@gmail.com>
2016-11-30 15:52:30 +0100

    Table view component formatter helper

--- EmberApp/app/controllers/dashboard/agents/all-agents.js
+++ EmberApp/app/controllers/dashboard/agents/all-agents.js
@@ -8,29 +8,17 @@ export default Ember.Controller.extend({
     offset: 10,
     colNames: ['ID', 'First Name', 'Last Name', ' Username', 'Agent Type', 'Country', 'Status', 'Actions'],
     colModels: Ember.computed('groupsModel', function () {
-        console.log(this.groupsModel);
         return [{value: 'id', compare:'cn'},
             {value: 'firstName', compare:'cn'},
             {value: 'lastName', compare:'cn'},
             {value: 'username', compare:'cn'},
             {value: 'group.name', compare:'eq', compareValues: this.groupsModel},
             {value: 'address.country', compare:'cn'},
-            {value: 'enabled', compare:'eq', compareValues: [{name: Translator.trans('All'), value: -1}, {name: Translator.trans('Enabled'), value: 1}, {name: Translator.trans('Not Enabled'), value: 0}]}
+            {value: 'enabled', compare:'eq', compareValues: [{name: Translator.trans('All'), value: -1}, {name: Translator.trans('Enabled'), value: 1}, {name: Translator.trans('Not Enabled'), value: 0}], formatter: function (value) {
+                return value ? Translator.trans('Enabled'): Translator.trans('Not enabled');
+            }}
         ]
     }),
-    init: function () {
-        this._super(...arguments);
-        // console.log(this.get('groups'));
-        //
-        // this.get('store').findAll('group').then((items)=> {
-        //     groups = items;
-        // });
-        // console.log(this.model);
-        // console.log(this.get('groups'));
-        // this.get('groups').forEach(function (item) {
-        //
-        // });
-    },
     actions: {
         filterModel: function (searchArray, page, column, sortType) {
             return this.get('store').query('agent', {
