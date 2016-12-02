ae874a11eac79366ac853e30092e3574aa7f7575
Nikola Ugrinovic <ugra1992@gmail.com>
2016-12-02 16:48:11 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	src/UserBundle/Controller/TCRUserController.php

--- EmberApp/app/components/agent-picker-component.js
+++ EmberApp/app/components/agent-picker-component.js
@@ -2,9 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     store: Ember.inject.service(),
+    currentUser: Ember.inject.service('current-user'),
     agents: [],
     selectedAgentIndex: -1,
     selectedAgentId: null,
+    agentsFiltered: Ember.computed('groups.@each.name', 'currentUser', function () {
+        let ctx = this;
+        if(this.get('currentUser.user.roles').indexOf('ROLE_SUPER_ADMIN') != -1){
+            return this.get('agents');
+        } else {
+            return [this.get('currentUser.user')];
+        }
+    }),
     init(){
         this._super(...arguments);
         this.set('agents', this.get('store').findAll('agent'));
