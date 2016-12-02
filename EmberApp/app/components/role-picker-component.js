80f2d62fe77adf9aa64d814500530fd23813cf83
Nikola Ugrinovic <ugra1992@gmail.com>
2016-12-02 15:24:35 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	src/UserBundle/Controller/TCRUserController.php

--- EmberApp/app/components/role-picker-component.js
+++ EmberApp/app/components/role-picker-component.js
@@ -4,16 +4,22 @@ export default Ember.Component.extend({
     store: Ember.inject.service(),
     currentUser: Ember.inject.service('current-user'),
     groups: [],
+    groupsFiltered: Ember.computed('groups.@each.name', 'currentUser', function () {
+        let ctx = this;
+        return this.get('groups').filter(function (item) {
+            return ctx.get('currentUser.user.roles').indexOf(item.get('name')) == -1;
+        });
+    }),
     selectedGroupIndex: -1,
     selectedGroup: null,
     init(){
         this._super(...arguments);
-        console.log(this.get('currentUser.user.group'));
         this.set('groups', this.get('store').findAll('group'));
         let index = (this.get('groups').indexOf(this.get('selectedGroup')));
         if(index !== -1){
             this.set('selectedGroupIndex', index);
         }
+        // console.log(this.get('currentUser.user.roles'));
     },
     actions: {
         roleChanged: function (groupIndex) {
