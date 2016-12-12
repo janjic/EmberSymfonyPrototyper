8b3ad09c76d5a92906320884b0bcc2ab9298bbcf
Nikola Ugrinovic <ugra1992@gmail.com>
2016-12-12 13:35:47 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	src/UserBundle/Controller/TCRUserController.php

--- EmberApp/app/models/thread.js
+++ EmberApp/app/models/thread.js
@@ -1,10 +1,11 @@
 import DS from 'ember-data';
 
-const {  Model } = DS;
+const {  attr, Model } = DS;
 
 export default Model.extend({
     createdBy:      DS.belongsTo('agent'),
     messages:       DS.hasMany('message'),
-    threadMetadata: DS.hasMany('threadMetadata')
+    threadMetadata: DS.hasMany('threadMetadata'),
+    createdAt:      attr('custom-date')
 
 });
