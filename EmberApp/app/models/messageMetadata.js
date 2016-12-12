f2300739896bb1351b1fe033a91223f1ab8650ee
pavle <loske92@gmail.com>
2016-12-12 13:38:42 +0100

    message start

--- EmberApp/app/models/messageMetadata.js
+++ EmberApp/app/models/messageMetadata.js
@@ -5,5 +5,5 @@ const { Model, attr } = DS;
 export default Model.extend({
     message:     DS.belongsTo('message', {inverse: 'messageMetadata'}),
     participant: DS.belongsTo('agent'),
-    $isRead:     attr('boolean')
+    isRead:      attr('boolean')
 });
