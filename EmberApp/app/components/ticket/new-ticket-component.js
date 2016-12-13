ad8c1dad633609e9db4e84f3543887088f66881d
Nikola Ugrinovic <ugra1992@gmail.com>
2016-12-13 09:27:34 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	EmberApp/app/models/message-metadata.js
    #	EmberApp/app/models/message.js
    #	EmberApp/app/models/thread-metadata.js
    #	EmberApp/app/models/thread.js

--- EmberApp/app/components/ticket/new-ticket-component.js
+++ EmberApp/app/components/ticket/new-ticket-component.js
@@ -1,18 +1,61 @@
 import Ember from 'ember';
+import LoadingStateMixin from '../../mixins/loading-state';
+import Changeset from 'ember-changeset';
+import lookupValidator from './../../utils/lookupValidator';
+import TicketValidations from '../../validations/ticket/add-ticket';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(LoadingStateMixin, {
+    TicketValidations,
     store: Ember.inject.service(),
     ticketTypes: ['BUG REPORT', 'WRONG ORDER', 'WRONG INQUIRY'],
     ticketType: 'BUG REPORT',
-
-    actions: {
-        chooseDestination(type) {
-            this.set('ticketType', type);
-        }
-    },
-
-    didInsertElement() {
+    init(){
         this._super(...arguments);
+        this.changeset = new Changeset(this.get('model'), lookupValidator(TicketValidations), TicketValidations);
+    },
+    actions: {
+        chooseType(type) {
+            this.set('ticketType', type);
+        },
+        saveTicket(ticket){
+            ticket.set('type', this.ticketType);
+            let changeset = this.get('changeset');
+            changeset.validate();
+            if(changeset.get('isValid')) {
+                this.showLoader('loading.sending.data');
+                ticket.save().then(() => {
+                    this.toast.success(Translator.trans('models.ticket.save.message'));
+                    // this.setLoadingText('loading.redirecting');
+                    // this.get('goToRoute')('dashboard.agents.all-agents');
+                    this.disableLoader();
 
+                }, (response) => {
+                    response.errors.forEach((error)=>{
+                        switch (parseInt(error.status)) {
+                            // case ApiCode.AGENT_ALREADY_EXIST:
+                            //     this.toast.error(Translator.trans('models.agent.unique.entity'));
+                            //     break;
+                            // case ApiCode.FILE_SAVING_ERROR:
+                            //     this.toast.error(Translator.trans('models.agent.file.error'));
+                            //     break;
+                            // case ApiCode.ERROR_MESSAGE:
+                            //     this.toast.error(error.message);
+                            //     break;
+                            // default:
+                            //     return;
+                        }
+                        this.disableLoader();
+                    });
+                });
+            }
+
+        },
+        /** validations */
+        reset(changeset) {
+            return changeset.rollback();
+        },
+        validateProperty(changeset, property) {
+            return changeset.validate(property);
+        },
     },
 });
---
+++ EmberApp/app/models/file.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+const { attr } = DS;
+
+export default DS.Model.extend({
+    name:           attr('string'),
+    webPath:        attr('string'),
+    filePath:       attr('string'),
+    base64Content:  attr('string')
+});
