247858dc8f19ea0a39c7d141acba0eae522528c2
djordjeknezevic <djordje@fsd.rs>
2016-12-21 15:42:20 +0100

    Table Component - First, last page, timeout for search

--- EmberApp/app/components/table-view-component.js
+++ EmberApp/app/components/table-view-component.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import LoadingStateMixin from '../mixins/loading-state';
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Component.extend(LoadingStateMixin, {
     sortColumn: 'id',
@@ -11,37 +12,15 @@ export default Ember.Component.extend(LoadingStateMixin, {
     searchArray: [],
     actions: {
         goToPage: function (page) {
-            page = (typeof page === 'string') ? (page === '+1' || page === '-1') ? (this.get('page') + parseInt(page)) : parseInt(page) : page;
-            if (1 <= page && page <= this.get('maxPages')) {
-                this.set('page', page);
+            let currentPage = this.get('page');
+            let maxPages = this.get('maxPages');
+            page = (typeof page === 'string') ? (page === '+1' || page === '-1') ? (this.get('page') + parseInt(page)) : page : page;
+            if ((page === '=1' && currentPage !== 1) || (page === '='+maxPages && currentPage !== maxPages) || (1 <= page && page <= maxPages)) {
+                this.set('page', page === '=1' ? 1 : page === '='+maxPages ? maxPages : parseInt(page));
                 this.loadData(this.get('paramsArray'));
             }
-        },
-        handleFilterEntry(column, searchValue, compareType) {
-            let searchArrayFields = this.get('searchArray');
-            let exists = searchArrayFields.findBy('field', column);
-            if (exists !== undefined) {
-                if (searchValue === '') {
-                    searchArrayFields.removeObject(exists);
-                } else {
-                    exists.data = searchValue;
-                }
-            } else {
-                if (searchValue !== '') {
-                    searchArrayFields.addObject({
-                        field: column,
-                        op: compareType,
-                        data: searchValue
-                    });
-                }
-            }
-            let paramsArray = this.get('paramsArray');
-            paramsArray.rules = searchArrayFields;
-            this.set('page', 1);
-            this.loadData(paramsArray);
         }
     },
-
     loadData: function (paramsArray){
         this.showLoader();
         let result = this.get('filter')(paramsArray, this.get('page'), this.get('sortColumn'), this.get('sortType'));
@@ -56,5 +35,29 @@ export default Ember.Component.extend(LoadingStateMixin, {
         } else {
             this.disableLoader();
         }
-    }
+    },
+    handleFilterEntry: task(function * (column, searchValue, compareType, delayTime) {
+        yield timeout(delayTime);
+        let searchArrayFields = this.get('searchArray');
+        let exists = searchArrayFields.findBy('field', column);
+        if (exists !== undefined) {
+            if (searchValue === '') {
+                searchArrayFields.removeObject(exists);
+            } else {
+                exists.data = searchValue;
+            }
+        } else {
+            if (searchValue !== '') {
+                searchArrayFields.addObject({
+                    field: column,
+                    op: compareType,
+                    data: searchValue
+                });
+            }
+        }
+        let paramsArray = this.get('paramsArray');
+        paramsArray.rules = searchArrayFields;
+        this.set('page', 1);
+        this.loadData(paramsArray);
+    }).restartable()
 });
