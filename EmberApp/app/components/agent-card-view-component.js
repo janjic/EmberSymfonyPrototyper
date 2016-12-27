54b392dc96e4e2d64cb8f3673dc9bc49cfc05c17
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2016-12-27 12:08:09 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	EmberApp/app/components/ticket/new-ticket-component.js
    #	EmberApp/app/templates/components/ticket/new-ticket-component.hbs

--- EmberApp/app/components/agent-card-view-component.js
+++ EmberApp/app/components/agent-card-view-component.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    firstBtnClasses: "button dark icon-btn",
+    secondBtnClasses: "button dark icon-btn",
+    inputClasses:  Ember.computed('focusable', function () {
+        if (!this.get('focusable')) {
+            return 'form-control input-lg search-input';
+        }
+        return 'form-control input-lg search-input clicked';
+
+    }),
+    limitAll: true,
+
+    actions: {
+        addClickedClass() {
+            this.set('focusable', true);
+        },
+        removeClickedClass (){
+            this.set('focusable', false);
+        },
+        setSearch (input) {
+            if (Object.is(parseInt(input), 1)) {
+                this.set('firstBtnClasses', "button dark icon-btn green");
+                this.set('secondBtnClasses', "button dark icon-btn");
+            } else {
+                this.set('firstBtnClasses', "button dark icon-btn");
+                this.set('secondBtnClasses', "button dark icon-btn green");
+            }
+            this.toggleProperty('limitAll');
+        }
+    }
 });
