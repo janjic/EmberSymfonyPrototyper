0d9faf5fbe0764223b452c4553ce83eb84f1ee5e
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2016-12-27 14:32:35 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	EmberApp/app/components/ticket/new-ticket-component.js
    #	EmberApp/app/templates/components/ticket/new-ticket-component.hbs

--- EmberApp/app/components/agent-card-view-component.js
+++ EmberApp/app/components/agent-card-view-component.js
@@ -1,6 +1,21 @@
 import Ember from 'ember';
-
-export default Ember.Component.extend({
+import { task, timeout } from 'ember-concurrency';
+import LoadingStateMixin from '../mixins/loading-state';
+export default Ember.Component.extend(LoadingStateMixin,{
+    limitAll: true,
+    sortType: 'asc',
+    defaultDelayTime: 250,
+    columns: ['username', 'firstName', 'lastName'],
+    compareType:'cn',
+    sortColumn: 'id',
+    searchValue: '',
+    defaultSortType: null,
+    showSearchSortBar: true,
+    paramsArray: {
+        groupOp: 'or',
+        rules: []
+    },
+    searchArray: [],
     firstBtnClasses: "button dark icon-btn",
     secondBtnClasses: "button dark icon-btn",
     inputClasses:  Ember.computed('focusable', function () {
@@ -10,9 +25,61 @@ export default Ember.Component.extend({
         return 'form-control input-lg search-input clicked';
 
     }),
-    limitAll: true,
+
+    loadData: function (paramsArray){
+        this.showLoader();
+        let result = this.get('filter')(paramsArray, this.get('page'), this.get('sortColumn'), this.get('sortType'));
+        if (result) {
+            result.then((filterResults) => {
+                this.set('model', filterResults);
+                this.set('maxPages', filterResults.meta.pages);
+                this.disableLoader();
+            }).catch(() => {
+                this.disableLoader();
+            });
+        } else {
+            this.disableLoader();
+        }
+    },
+    handleFilterEntry: task(function * () {
+        let searchValue = this.get('searchValue').trim();
+        let delayTime =  this.get('defaultDelayTime');
+        yield timeout(delayTime);
+        let searchArrayFields = this.get('searchArray');
+        if (searchValue !== '') {
+            this.get('columns').forEach((column) => {
+                searchArrayFields.addObject({
+                    field: column,
+                    op: this.get('compareType'),
+                    data: searchValue
+                });
+            });
+        }
+        let paramsArray = this.get('paramsArray');
+        paramsArray.rules = searchArrayFields;
+        this.set('page', 1);
+        this.loadData(paramsArray);
+        this.set('searchArray', []);
+    }).restartable(),
+
 
     actions: {
+        handlePageChange(page) {
+            this.send('goToPage', page);
+        },
+        handleOffsetChange() {
+            this.set('offset', this.$('select')[0].value);
+            this.get('filter')(1);
+        },
+        goToPage(page) {
+            let currentPage = this.get('page');
+            let maxPages = this.get('maxPages');
+            page = (typeof page === 'string') ? (page === '+1' || page === '-1') ? (this.get('page') + parseInt(page)) : page : page;
+            if ((page === '=1' && currentPage !== 1) || (page === '='+maxPages && currentPage !== maxPages) || (1 <= page && page <= maxPages)) {
+                this.set('page', page === '=1' ? 1 : page === '='+maxPages ? maxPages : parseInt(page));
+                this.loadData(this.get('paramsArray'));
+            }
+        },
         addClickedClass() {
             this.set('focusable', true);
         },
