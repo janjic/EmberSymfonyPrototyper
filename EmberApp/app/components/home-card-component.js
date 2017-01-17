1241df50caf07653d2f0a92c8a62bd65edc7ffce
filip106 <djordjevic.filip994@gmail.com>
2017-01-17 09:41:31 +0100

    Fixed counter for dashboard.

--- EmberApp/app/components/home-card-component.js
+++ EmberApp/app/components/home-card-component.js
@@ -3,19 +3,46 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     currencyEUR : true,
 
+    didRender() {
+        this._super(...arguments);
+        let refreshFunc = Ember.run.schedule('sync', this, function() {
+            let ctx = this;
+            ctx.$('.timer').each(function () {
+                ctx.$(this).prop('Counter',0).animate({
+                    Counter: ctx.$(this).text()
+                }, {
+                    duration: 700,
+                    easing: 'swing',
+                    step: function (now) {
+                        ctx.$(this) ? ctx.$(this).text(Math.ceil(now)) : window.$('.timer').stop();
+                    }
+                });
+            });
+        });
+        this.set('refresh', refreshFunc);
+
+    },
     didInsertElement(){
         this._super(...arguments);
         this.get('getNewElements')();
-        let ctx = this;
         if ( this.get('isMultipleCard') ){
-            let id = window.setInterval(function () {
-                ctx.set('currencyEUR', !ctx.get('currencyEUR'));
-            }, 4000);
-            this.set('intervalId', id);
+            this.changeCurrency();
         }
     },
 
+    changeCurrency(){
+        let ctx = this;
+        let id = Ember.run.later(this, function () {
+            ctx.set('currencyEUR', !ctx.get('currencyEUR'));
+            ctx.changeCurrency();
+        }, 4000);
+        this.set('intervalId', id);
+    },
+
     willDestroy(){
-      window.clearInterval(this.get('intervalId'));
+        Ember.run.cancel(this.get('intervalId'));
+        Ember.run.cancel(this.get('refresh'));
+        this.set('refresh', undefined);
+        window.$('.timer').stop();
     }
 });
