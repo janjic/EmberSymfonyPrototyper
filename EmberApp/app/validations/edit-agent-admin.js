a3ef3f64397d2df3b5f6f7c4e4406ce82e2278d1
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2017-01-20 11:59:02 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	EmberApp/config/environment.js

--- EmberApp/app/validations/edit-agent-admin.js
+++ EmberApp/app/validations/edit-agent-admin.js
@@ -4,14 +4,12 @@ import Ember from 'ember';
 const { Translator } = window;
 import validateIsSame  from '../validators/same';
 import {
-    validatePresence,
     validateFormat
 } from 'ember-changeset-validations/validators';
 const { assign } = Ember;
 const EditAgentValidation = {
     plainPassword:  validateFormat({regex: /^($|.{6,})$/,      message: Translator.trans('validations.presence-password')}),
     passwordRepeat: validateIsSame({on: 'plainPassword', message: Translator.trans('validations.password-repeat')}),
-    superior:       validatePresence(true),
 };
 export default assign({},AgentValidation, EditAgentValidation);
 
