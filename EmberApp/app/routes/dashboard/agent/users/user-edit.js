af8b386c0c9b95a0af8a61d2d52d575fab4afdf9
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2017-01-22 10:06:18 +0100

    Merge branch 'develop' of https://github.com/janjic/EmberSymfonyPrototyper into develop
    # Conflicts:
    #	EmberApp/config/environment.js

--- EmberApp/app/routes/dashboard/agent/users/user-edit.js
+++ EmberApp/app/routes/dashboard/agent/users/user-edit.js
@@ -4,3 +4,25 @@ export default Ember.Route.extend({
         return this.store.findRecord('tcrUser', params.id, {reload: true});
     }
 });
+
+import Ember from 'ember';
+const {
+    inject: {
+        service
+    }
+} = Ember;
+
+export default Ember.Route.extend({
+    currentUser: service('current-user'),
+    model(params) {
+        return this.store.findRecord('tcrUser', params.id);
+    },
+
+    afterModel(model) {
+        if (!Object.is(model.get('agent.id'), this.get('currentUser.user.id'))) {
+            this.transitionTo('dashboard.agent.users.users-customers');
+        }
+
+    }
+
+});
