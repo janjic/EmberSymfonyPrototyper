f975c7dd4e99f36c3af3e442d2bcd589e8db2fd2
Nikola Ugrinovic <ugra1992@gmail.com>
2017-01-30 12:20:51 +0100

    Agent history table changed with timeline and agent select

--- EmberApp/app/controllers/dashboard/agents/agent-update-history.js
+++ EmberApp/app/controllers/dashboard/agents/agent-update-history.js
@@ -1,62 +1,34 @@
 import Ember from 'ember';
 const Translator = window.Translator;
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
-    updateTypes: [{name: 'Upgrade', key: 'UPGRADE'}, {name: 'Downgrade', key: 'DOWNGRADE'}, {name: 'Suspension', key: 'SUSPENSION'}],
-    currentUpdateType: {name: 'Upgrade', key: 'UPGRADE'},
-
-    store: Ember.inject.service(),
-    eventBus: Ember.inject.service('event-bus'),
-
-    page: 1,
-    offset: 10,
-    colNames: [
-        'admin-agent.history.agent.id',
-        'admin-agent.history.agent.name',
-        'admin-agent.history.changed.by',
-        'admin-agent.history.changed.from',
-        'admin-agent.history.changed.to',
-        'admin-agent.history.is.suspended',
-        'admin-agent.history.date',
-        'admin-agent.history.actions',
-    ],
-
-    colModels: [
-        {value: 'agent.id', compare:'cn'},
-        {value: 'agent.fullName', compare:'cn'},
-        {value: 'changedByAgent.fullName', compare:'cn'},
-        {value: 'changedFrom.name', compare:'cn'},
-        {value: 'changedTo.name', compare:'cn'},
-        {value: 'changedToSuspended', compare:'eq', compareValues: [
-            {name: Translator.trans('admin-agent.history.all'), value: -1},
-            {name: Translator.trans('admin-agent.history.suspended'), value: 1},
-            {name: Translator.trans('admin-agent.history.not.suspended'), value: 0}
-        ], formatter: function (value) {
-            if (value === false) {
-                return Translator.trans('admin-agent.history.not.suspended');
-            }
-            return Translator.trans('admin-agent.history.suspended');
-        }},
-        {value: 'date', compare:'cn', formatter: function (value) {
-            return value.split('.')[0];
-        }}
-    ],
-
     actions: {
-        filterModel: function (searchArray, page, column, sortType) {
-            return this.get('store').query('agent-history', {
-                page: page,
-                offset: this.get('offset'),
-                sidx: column,
-                sord: sortType,
-                filters: JSON.stringify(searchArray),
-                type: this.get('currentUpdateType.key')
-            });
-        },
+        agentSelected(agent){
+            if(agent !== null){
+                this.set('isLoading', true);
+                this.get('store').findRecord('agent', agent.get('id'), { reload : true }).then((agent)=>{
+                    this.store.query('agent-history', { agentId: agent.get('id')}, { reload : true }).then((history)=>{
+                        this.set('agentHistory',history);
+                        this.set('currentAgent', agent);
+                        this.set('isLoading', false);
+                    });
+                });
+            } else {
+                this.set('currentAgent', null);
+                this.set('agentHistory', []);
+            }
 
-        updateTypeChanged(type) {
-            this.set('currentUpdateType', type);
-            this.get('eventBus').publish('resetTableEvent');
-        }
+        },
+    },
+    search: task(function * (text, page, perPage) {
+        yield timeout(200);
+        return this.get('store').query('agent', {page:page, rows:perPage, search: text, searchField: 'agent.email'}).then(results => results);
+    }),
+
+    init(){
+        this.set('currentAgent', null);
+        this.set('agentHistory', []);
+        this.set('isLoading', false);
     }
 });
