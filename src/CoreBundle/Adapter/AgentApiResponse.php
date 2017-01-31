25d1e91d917e90b6170231d424c48039a5f2fd79
pavle <loske92@gmail.com>
2017-01-31 11:46:07 +0100

    Agent view and edit security

--- src/CoreBundle/Adapter/AgentApiResponse.php
+++ src/CoreBundle/Adapter/AgentApiResponse.php
@@ -16,6 +16,7 @@ class AgentApiResponse
     const PASSWORDS_CHANGED_OK_RESPONSE         = array('status' => AgentApiCode::PASSWORDS_CHANGED_OK);
     const OLD_PASSWORD_IS_NOT_CORRECT_RESPONSE  = array('status' => AgentApiCode::OLD_PASSWORD_IS_NOT_CORRECT);
     const AGENT_NOT_FOUND_RESPONSE              = array('status' => AgentApiCode::AGENT_NOT_FOUND);
+    const AGENT_ACCESS_DENIED                   = array('errors' => array(array('status' => AgentApiCode::ACCESS_DENIED)));
 
     const AGENT_ALREADY_EXIST                   = array('errors' => array(array('status'=> AgentApiCode::AGENT_ALREADY_EXIST)));
     const AGENT_SAVED_FILE_FAILED_RESPONSE      = array('errors' => array(array('status'=> AgentApiCode::FILE_SAVING_ERROR)));
