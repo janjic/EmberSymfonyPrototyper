f3118561a468c791fee3b7c8b8f0562ad38dea45
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2017-02-01 12:05:02 +0100

    Bug fixes.

--- EmberApp/app/helpers/title-set.js
+++ EmberApp/app/helpers/title-set.js
@@ -1,12 +1,7 @@
 import Ember from 'ember';
 const Translator = window.Translator;
-const {
-    inject: {
-        service
-    }
-} = Ember;
+
 export default Ember.Helper.extend({
-    session: service('session'),
     compute() {
         Ember.$(document).attr('title', Translator.trans('meta.title'));
     }
