423557702810de3da0879d4ab874674166637b97
Branko Janjic <brankojanjic@Brankos-MacBook-Air-2.local>
2017-02-09 15:36:36 +0100

    Bug fixes.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Repository\RepositoryFactory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
